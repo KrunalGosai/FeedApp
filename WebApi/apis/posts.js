39d339fb05350c1e451f863422ac1c6e38348523
Krunal Gosai <krunal.gosai@binaryrepublik.com>
2018-06-08 06:11:29 -0700

    new list api with like and love activity

--- WebApi/apis/posts.js
+++ WebApi/apis/posts.js
@@ -23,13 +23,56 @@ express = require('express')
         }
     })
     
-    router.get('/api/feed/list',(req,res) => {
+    router.get('/api/feed/list_old',(req,res) => {
         models.feedPosts.find({}).sort({postedTime: 'descending'}).limit( 10 ).then((resobj) =>{
             res.send({list:resobj});
             console.log('list operation completed.')
         },(err)=>{
             console.log(err);
         });
+    })
+
+    router.get('/api/feed/list',(req,res) => {
+        models.feedPosts.aggregate([
+            { $sort : { postedTime : -1}},
+            { $limit : 10 },
+            {
+                "$lookup": {
+                    from: "postactivities",
+                    localField: "_id",
+                    foreignField: "post_id",
+                    as: "postLike"
+                }
+            },
+            {
+                "$lookup": {
+                    from: "postactivities",
+                    localField: "_id",
+                    foreignField: "post_id",
+                    as: "postLove"
+                }
+            },
+            {
+                $project: {
+                    user_id: 1,feedPost:1,postedTime:1,imgUrl:1,_id:1,
+                    postLike: { 
+                        $filter: { 
+                            input: "$postLike", 
+                            as: "postLike", 
+                            cond: { $eq : [ "$$postLike.activity", "like" ] } 
+                        } 
+                    },
+                    postLove: { 
+                        $filter: { 
+                            input: "$postLove", 
+                            as: "postLove", 
+                            cond: { $eq : [ "$$postLove.activity", "love" ] } 
+                        } 
+                    } 
+                } 
+            } 
+            
+        ]).then((data) => {res.send({list:data}), console.log('list operation completed.')},(err) => {console.log(err)});
     })
 
 module.exports = router;
--- WebApi/models.js
+++ WebApi/models.js
@@ -1,8 +1,10 @@
 const mongo = require('Mongoose');
+const Schema = mongo.Schema;
     mongo.connect('mongodb+srv://krunal:mongodev@cluster0-v8st9.mongodb.net/feedapp?retryWrites=true');
 const models = {
     feedPosts: mongo.model('feedPost',  {user_id: String,feedPost: String,postedTime: Date,imgUrl:String}),
-    users: mongo.model('user',{firstName:String,lastName:String,gender:Boolean,dob:Date})
+    users: mongo.model('user',{firstName:String,lastName:String,gender:Boolean,dob:Date}),
+    postActivitys: mongo.model('postActivity',{ activity:String,user_id:String,post_id:{type:Schema.Types.ObjectId, ref:'postActivity'}})
 };
 
 module.exports = models;
